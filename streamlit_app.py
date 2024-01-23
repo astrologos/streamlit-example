846cd3d108674089759169a4ce37373245e90268
Jack Alvarez <jalvarezm1995@outlook.com>
2024-01-22 20:30:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import base64
 
-st.title('Viewport-Fitted and Zoomable PDF Viewer')
+st.title('Responsive PDF Viewer')
 
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
@@ -10,37 +10,23 @@ if uploaded_file is not None:
     pdf_file = uploaded_file.read()
     b64_pdf = base64.b64encode(pdf_file).decode('utf-8')
 
-    # HTML and JavaScript for zoomable PDF viewer and control buttons
+    # HTML and JavaScript for a responsive PDF viewer
     pdf_display = f'''
     <html>
     <head>
         <script>
-            function zoomIn() {{
-                var iframe = document.getElementById('pdf-iframe');
-                var currWidth = iframe.clientWidth;
-                iframe.style.width = (currWidth + 100) + "px";
-            }}
-            function zoomOut() {{
-                var iframe = document.getElementById('pdf-iframe');
-                var currWidth = iframe.clientWidth;
-                if(currWidth > 100) {{
-                    iframe.style.width = (currWidth - 100) + "px";
-                }}
-            }}
-            function resetZoom() {{
-                var iframe = document.getElementById('pdf-iframe');
-                iframe.style.width = "100%";
+            function resizeIframe(obj) {{
+                obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
             }}
         </script>
     </head>
     <body>
-        <div style="text-align: center;">
-            <button onclick="zoomIn()">Zoom In</button>
-            <button onclick="zoomOut()">Zoom Out</button>
-            <button onclick="resetZoom()">Reset Zoom</button>
-        </div>
-        <div style="height: calc(100vh - 150px); overflow-y: auto;">
-            <iframe id="pdf-iframe" src="data:application/pdf;base64,{b64_pdf}" style="width:100%; height:100%;" frameborder="0" scrolling="yes"></iframe>
+        <div style="overflow-y: auto; height: calc(100vh - 150px);">
+            <iframe src="data:application/pdf;base64,{b64_pdf}" 
+                    style="width:100%; border:none;" 
+                    onload="resizeIframe(this)" 
+                    scrolling="yes" 
+                    id="pdf-iframe"></iframe>
         </div>
     </body>
     </html>
