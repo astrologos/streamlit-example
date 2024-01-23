ba8d0402572a63f22a86b72d6fddd39a24448f82
Jack Alvarez <jalvarezm1995@outlook.com>
2024-01-22 20:23:28 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import base64
 
-st.title('Responsive PDF Viewer')
+st.title('Viewport-Fitted PDF Viewer')
 
 uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
@@ -10,10 +10,10 @@ if uploaded_file is not None:
     pdf_file = uploaded_file.read()
     b64_pdf = base64.b64encode(pdf_file).decode('utf-8')
 
-    # HTML to embed PDF in a responsive container
+    # HTML to embed PDF in a fully responsive container
     pdf_display = f'''
-    <div style="height: 80vh; width: 100%; overflow-y: scroll;">
-        <iframe width="100%" height="100%" src="data:application/pdf;base64,{b64_pdf}" type="application/pdf"></iframe>
+    <div style="height: calc(100vh - 150px); overflow-y: auto;">
+        <iframe src="data:application/pdf;base64,{b64_pdf}" style="width:100%; height:100%;" frameborder="0" scrolling="yes"></iframe>
     </div>
     '''
     st.markdown(pdf_display, unsafe_allow_html=True)
